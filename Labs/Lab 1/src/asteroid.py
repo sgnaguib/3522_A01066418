22889dd61490e94564b217636bd6cb1ccff62128
sgnaguib <sgnaguib@gmail.com>
2019-09-12 18:40:34 -0700

    Comments added and improved

--- Labs/Lab 1/src/asteroid.py
+++ Labs/Lab 1/src/asteroid.py
@@ -4,44 +4,74 @@ from datetime import datetime
 
 
 class Asteroid():
+    """
+    Defines an asteroid that can be moved.
+    """
 
     id_count = 0
 
     def __init__(self, radius, position, velocity, timestamp):
+        """
+        Constructs Asteroid and gives it a unique ID.
+        :param radius: radius of asteroid
+        :param position: a list of x,y,z coordinates of asteroid
+        :param velocity: a list of x,y,z velocity of asteroid
+        :param timestamp: timestamp of when asteroid was created
+        """
         self.radius = radius
         self.position = position
         self.velocity = velocity
-        self.time_created = timestamp
-        self.id = Asteroid.id_count
+        self._time_created = timestamp
+        self._id = Asteroid.id_count
         Asteroid.id_count += 1
 
     def set_radius(self, value):
+        """
+        :param value: new asteroid radius.
+        """
         self.radius = value
 
     def get_radius(self):
+        """
+        :return: asteroid's radius.
+        """
         return self.radius
 
     def set_position(self, value):
+        """
+        :param value: new asteroid position.
+        """
         self.position = value
 
-    def get_position(self, value):
+    def get_position(self):
+        """
+        :return: asteroid's current position.
+        """
         return self.position
 
     def set_velocity(self, value):
+        """
+        :param value: asteroid's new velocity.
+        """
         self.velocity = value
 
     def get_velocity(self):
+        """
+        :return: asteroid's current velocity.
+        """
         return self.velocity
 
-    def set_timestamp(self, value):
-        self.timestamp = value
-
     def get_timestamp(self):
-        return self.timestamp
+        """
+        :return: time that asteroid was created.
+        """
+        return self._time_created
 
     def move(self):
-        # Modifies asteroid position using velocity
-        # Returns the new position as a tuple of x,y, and z coordinates
+        """
+        Modifies asteroid position using velocity.
+        :returns: the new position as a tuple of x,y, and z coordinates
+        """
         self.position[0] += self.velocity[0]
         self.position[1] += self.velocity[1]
         self.position[2] += self.velocity[2]
@@ -49,8 +79,13 @@ class Asteroid():
 
     @classmethod
     def rand_asteroid(cls, max_velocity, max_position, max_radius):
-        # Creates an asteroid with a random radius between [1,4], within a random position in a
-        # cube 100 meters per side, with a velocity no greater than 5 m/s in any direction
+        """
+        Creates an asteroid with a random radius, position, and velocity.
+        :param max_velocity: maximum velocity of asteroid
+        :param max_position: maximum position of asteroid
+        :param max_radius: maximum radius of asteroid
+        :return: newly created asteroid
+        """
         rand_vx = randint(-max_velocity, max_velocity)
         rand_vy = randint(-max_velocity, max_velocity)
         rand_vz = randint(-max_velocity, max_velocity)
@@ -60,7 +95,10 @@ class Asteroid():
         return Asteroid(random_radius, random_position, velocity, datetime.now())
 
     def __str__(self):
-        return f"Asteroid id: {self.id}, position: {self.position},"\
-                f"velocity: {self.velocity}, time created: {self.time_created}"
+        """
+        :return: Description of Asteroid's attributes
+        """
+        return f"Asteroid id: {self._id}, position: {self.position},"\
+                f"velocity: {self.velocity}, time created: {self._time_created}"
 
 
--- Labs/Lab 1/src/controller.py
+++ Labs/Lab 1/src/controller.py
@@ -1,25 +1,32 @@
 from asteroid import Asteroid
-from random import randrange
 import datetime
 import time
 
 class Controller:
     """
-    Cont
+    Controls the creation and movement of Asteroid objects.
     """
 
     def __init__(self):
+        """
+        Creates a hundred asteroid objects and stores them in a list.
+        """
         self.asteroid_list = []
         for index in range(0, 100):
             self.asteroid_list.append(Asteroid.rand_asteroid(5,100,4))
 
     def simulate(self, seconds):
+        """
+        Starting at the beginning of a new second on the computer's clock, this method
+        moves all asteroids once a second for the number of seconds specified by the parameter
+        :param seconds: the number of times the asteroids will be moved
+        """
 
         seconds_count = 0
         comp_time = datetime.datetime.now().microsecond
         wait_time = 1 - comp_time/1e6
         time.sleep(wait_time)
-        while(seconds_count < seconds):
+        while seconds_count < seconds:
             time.sleep(1)
             print(f"\nAfter {seconds_count+1} seconds")
             for element in self.asteroid_list:
@@ -30,6 +37,9 @@ class Controller:
 
 
 def main():
+    """
+    Initiates a controller object and drives the program
+    """
     test_controller = Controller()
     for element in test_controller.asteroid_list:
         print(element)
