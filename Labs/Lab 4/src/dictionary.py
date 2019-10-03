f2229a4f6fb4afdb59d374663cc83a86578fa6b0
sgnaguib <sgnaguib@gmail.com>
2019-10-02 21:15:22 -0700

    implemented load dictionary

--- Labs/Lab 4/src/dictionary.py
+++ Labs/Lab 4/src/dictionary.py
@@ -1,6 +1,7 @@
 from file_handler import FileHandler
 from file_handler import FileExtensions
 from pathlib import Path
+import json
 """
 Modules is responsible for housing the dictionary class and
 the driver methods
@@ -12,11 +13,16 @@ class Dictionary:
     """
 
     """
+
+    def __init__(self):
+        self.entries = {}
     def load_dictionary(self, filepath):
         """Responsible for loading
         data into the dictionary
         """
-        pass
+        self.entries = FileHandler.load_data(filepath,
+                                             FileExtensions.JSON)
+        print(self.entries["acidity"])
 
     def query_definition(self, word):
         """
@@ -27,9 +33,11 @@ class Dictionary:
 
 
 def main():
-    FileHandler.load_data(Path.cwd()/'data.json', FileExtensions.JSON)
-    FileHandler.write_lines(Path.cwd()/'check.txt', "Does this work?\n"
-                                                    "really?")
+    # FileHandler.load_data(Path.cwd()/'data.json', FileExtensions.JSON)
+    # FileHandler.write_lines(Path.cwd()/'check.txt', "Does this work?\n"
+    #                                                 "really?")
+    dictionary = Dictionary()
+    dictionary.load_dictionary(Path.cwd()/'data.json')
 
 
 if __name__ == '__main__':
