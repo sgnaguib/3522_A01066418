e576958b7071e9c5e8e10a31b0290082445f45e4
sgnaguib <sgnaguib@gmail.com>
2019-10-10 20:37:59 -0700

    Implemented highest bidder dictionary

--- Labs/Lab 5/auction_simulator.py
+++ Labs/Lab 5/auction_simulator.py
@@ -21,11 +21,16 @@ class Auction:
               f"${round(self.starting_price,1)}")
         self.auctioneer.place_bid(self.starting_price, None)
 
-
     def print_winner(self):
         print(f"The winner of the auction is "
               f"{self.auctioneer.highest_current_bidder} at "
               f"${round(self.auctioneer.highest_current_bid, 1)}")
+
+    def print_top_bids(self):
+        print("Highest bids Per Bidder")
+        top_bids = self.auctioneer.bidder_dictionary()
+        for bidder in self.bidders:
+            print(f"Bidder: {bidder}, Highest Bid: {top_bids[bidder]}")
 
 
 class Auctioneer:
@@ -47,10 +52,14 @@ class Auctioneer:
         :return:
         """
         if int(amount) > self.highest_current_bid:
-            if bidder is not None:
+            if bidder is not None and self.highest_current_bidder is not None:
                 print(f"{bidder} bid {round(amount,1)} in "
-                      f"response to {self.highest_current_bidder}'s"
-                      f" bid of ${round(self.highest_current_bid,1)}")
+                    f"response to {self.highest_current_bidder}'s"
+                    f" bid of ${round(self.highest_current_bid,1)}")
+            elif self.highest_current_bidder is not None:
+                print(f"{bidder} bid {round(amount, 1)} in "
+                      f"response to starting bid of"
+                      f" ${round(self.highest_current_bid, 1)}")
             self.highest_current_bid = amount
             self.highest_current_bidder = bidder
             self.notify_bidders(bidder)
@@ -64,6 +73,11 @@ class Auctioneer:
         for bidder in self.bidders:
             if bidder is not latest_bidder:
                 bidder(self)
+
+    def bidder_dictionary(self):
+        top_bids = {bidder: bidder.highest_bid for bidder
+                    in self.bidders}
+        return top_bids
 
 
 class Bidder:
@@ -95,9 +109,11 @@ class Bidder:
         if self is not auctioneer.highest_current_bidder and \
                 self.budget > auctioneer.highest_current_bid and \
                 random.random() < self.bid_probability:
-
-            auctioneer.place_bid(auctioneer.highest_current_bid *
-                                 self.bid_increase_perc, self)
+            bid = auctioneer.highest_current_bid * \
+                                 self.bid_increase_perc
+            if bid > self.highest_bid:
+                self.highest_bid = bid
+            auctioneer.place_bid(bid, self)
 
     def __str__(self):
         return self.name
@@ -124,6 +140,7 @@ def main():
     my_auction = Auction(bidder_list, item_name, starting_price)
     my_auction.place_first_bid()
     my_auction.print_winner()
+    my_auction.print_top_bids()
 
 
 if __name__ == "__main__":
